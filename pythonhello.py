b339e8ba51f5ee7769c6ae267e6ea874f5cfa734
Swapna128 <talishettyuma@gmail.com>
2025-03-29 13:08:20 +0530

    Add files via upload

--- pythonhello.py
+++ pythonhello.py
@@ -1,1 +1,12 @@
+import sys
 print("HEY HELLOOOOOOO");
+n1=int(sys.argv[1])
+n2=int(sys.argv[2])
+r1=n1+n2
+r2=n1-n2
+r3=n1*n2
+r4=n1/n2
+print("addition:",r1)
+print("subraction:",r2)
+print("multiplivation:",r3)
+print("division:",r4)
